71a8cd7616076b871f4aa051b0ec8fa766780438
Riddhi Bahuguna <riddhibahuguna@gmail.com>
2026-09-26 12:58:47 +0530

    add review schema to listing schema

--- backend/models/listing.js
+++ backend/models/listing.js
@@ -22,6 +22,13 @@ const listingSchema = new Schema({
   price: Number,
   country: String,
   location: String,
+
+  reviews: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: "Review",
+  }
+],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
